e462739dc48445281c65ce6257fd7390dbbd527e
Daniel Clarke <deliotclarke@gmail.com>
2019-05-21 18:29:28 -0500

    killed the import in candylist that was making it error out

--- src/components/candy/CandyList.js
+++ src/components/candy/CandyList.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react'
-import CandyTypes from '../API/candytypes.js'
 
 export default class Candy extends Component {
-
-  state = {
-    candyTypes: CandyTypes
-  }
 
   render() {
     return (
